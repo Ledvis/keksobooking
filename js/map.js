81097cb53e928d5799cbb3d53149b67fb61c9477
Zhenya <fedyai.eugen@gmail.com>
2018-04-11 12:15:22 +0300

    Refactor modules #2

--- js/map.js
+++ js/map.js
@@ -1,13 +1,21 @@
 'use strict';
 
-(function() {
+(function () {
   // DOM elements
   let mapEl = document.querySelector('.map');
   let mapPinsListEl = mapEl.querySelector('.map__pins');
   let mapMainPinEl = mapEl.querySelector('.map__pin--main');
   let mapPinTemplate = document.querySelector('template').content.querySelector('.map__pin');
 
-  const hideOfferInfo = function() {
+  const deleteOfferInfo = function (pin) {
+    let activatePin = pin;
+    activatePin.classList.remove('popup__pin--active');
+    let mapCard = mapEl.querySelector('.map__card');
+    mapCard.parentNode.removeChild(mapCard);
+    mapEl.removeEventListener('keydown', popUpEscHandler);
+  };
+
+  const clearOfferInfo = function () {
     let mapCard = mapEl.querySelector('.map__card');
     if (mapCard) {
       mapCard.parentNode.removeChild(mapCard);
@@ -15,15 +23,17 @@
     }
   };
 
-  const showOfferInfo = function(index) {
-    hideOfferInfo();
+  const showOfferInfo = function (index, pin) {
+    clearOfferInfo();
     let mapCard = window.popup.createMapCard(window.offer.offersList[index]);
     let mapCardCloseEl = mapCard.querySelector('.popup__close');
-    mapCardCloseEl.addEventListener('click', hideOfferInfo);
+    mapCardCloseEl.addEventListener('click', function () {
+      deleteOfferInfo(pin);
+    });
     mapEl.appendChild(mapCard);
   };
 
-  const mapClickHandler = function(evt) {
+  const mapClickHandler = function (evt) {
     let clickedEl = evt.target;
     if (!clickedEl.hasAttribute('data-pin')) {
       clickedEl = clickedEl.parentNode;
@@ -31,19 +41,24 @@
 
     let clickedIndex = clickedEl.getAttribute('data-pin');
     if (clickedIndex) {
-      showOfferInfo(clickedIndex);
-      clickedEl.classList.add('.popup__pin--active');
+      let activatePin = mapEl.querySelector('.popup__pin--active');
+      if (activatePin) {
+        activatePin.classList.remove('popup__pin--active');
+      }
+
+      clickedEl.classList.add('popup__pin--active');
+      showOfferInfo(clickedIndex, clickedEl);
     }
   };
 
-  const popUpEscHandler = function(evt) {
-    window.util.isEscEvent(evt, hideOfferInfo);
+  const popUpEscHandler = function (evt) {
+    window.util.isEscEvent(evt, clearOfferInfo);
   };
 
   mapEl.addEventListener('click', mapClickHandler, true);
   mapEl.addEventListener('keydown', popUpEscHandler, true);
 
-  const generatePins = function(data) {
+  const generatePins = function (data) {
     let fragment = document.createDocumentFragment();
 
     for (let i = 0; i < data.length; i++) {
@@ -59,7 +74,7 @@
     mapPinsListEl.appendChild(fragment);
   };
 
-  const activatePage = function() {
+  const activatePage = function () {
     mapEl.classList.remove('map--faded');
     window.form.enableForm();
     window.offer.generateOffers(window.consts.OFFERS_QUANTITY);
