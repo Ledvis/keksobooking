1264c7a83fe377b529ab99f1f4e62b1507ab4c02
Zhenya <fedyai.eugen@gmail.com>
2018-04-13 16:57:50 +0300

    Create new refactored module pin instead of drag

---
+++ js/pin.js
@@ -0,0 +1,72 @@
+'use strict';
+
+(function() {
+  const MapLimit = {
+    TOP: 100,
+    RIGHT: 1200,
+    BOTTOM: 450,
+    LEFT: 0
+  };
+
+  const MAIN_PIN_ARROW_CORRECTION = 50;
+
+  let mainPinEl = document.querySelector('.map__pin--main');
+
+  const onMouseDownClick = function(downEvt) {
+    downEvt.preventDefault();
+
+    let startPinCoord = {
+      x: downEvt.clientX,
+      y: downEvt.clientY
+    };
+
+    const onMouseMove = function(moveEvt) {
+      moveEvt.preventDefault();
+
+      let shift = {
+        x: startPinCoord.x - moveEvt.clientX,
+        y: startPinCoord.y - moveEvt.clientY
+      };
+
+      startPinCoord = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      let shiftedPinPosition = {
+        left: mainPinEl.offsetLeft - shift.x,
+        top: mainPinEl.offsetTop - shift.y
+      };
+
+      // vertical move
+      if (shiftedPinPosition.top > MapLimit.BOTTOM) {
+        mainPinEl.style.top = MapLimit.BOTTOM + 'px';
+      } else if (shiftedPinPosition.top < MapLimit.TOP) {
+        mainPinEl.style.top = MapLimit.TOP + 'px';
+      } else {
+        mainPinEl.style.top = shiftedPinPosition.top + 'px';
+      }
+
+      // horizontal move
+      if (shiftedPinPosition.left > MapLimit.RIGHT) {
+        mainPinEl.style.left = MapLimit.RIGHT + 'px';
+      } else if (shiftedPinPosition.left < MapLimit.LEFT) {
+        mainPinEl.style.left = MapLimit.LEFT + 'px';
+      } else {
+        mainPinEl.style.left = shiftedPinPosition.left + 'px';
+      }
+    };
+
+    const onMouseUp = function(upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  };
+
+  mainPinEl.addEventListener('mousedown', onMouseDownClick);
+})();
