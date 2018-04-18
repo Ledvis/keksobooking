221b14d1329e571defd3a0ca2cbe0b61d3c52938
Zhenya <fedyai.eugen@gmail.com>
2018-04-18 15:38:36 +0300

    Refactor module form

--- js/form.js
+++ js/form.js
@@ -1,15 +1,21 @@
 'use strict';
 
 (function() {
-  let formEl = document.querySelector('.notice__form');
-  let fieldsetEls = formEl.querySelectorAll('fieldset');
+  const INPUT_DEFAULT_BORDER_COLOR = '#d9d9d3';
+  const FORM_DISABLED_CLASS = 'notice__form--disabled';
+  const formEl = document.querySelector('.notice__form');
+  const fieldsetsEl = formEl.querySelectorAll('fieldset');
+  const titleEl = formEl.querySelector('#title');
+  const priceEl = formEl.querySelector('#price');
+  const timeinEl = formEl.querySelector('#timein');
+  const timeoutEl = formEl.querySelector('#timeout');
 
   const enableForm = function() {
-    formEl.classList.remove('notice__form--disabled');
+    formEl.classList.remove(FORM_DISABLED_CLASS);
     let fieldsetEl;
 
-    for (let i = 0; i < fieldsetEls.length; i++) {
-      fieldsetEl = fieldsetEls[i];
+    for (let i = 0; i < fieldsetsEl.length; i++) {
+      fieldsetEl = fieldsetsEl[i];
       fieldsetEl.removeAttribute('disabled');
     }
   };
@@ -18,11 +24,59 @@
     formEl.classList.add('notice__form--disabled');
     let fieldsetEl;
 
-    for (let i = 0; i < fieldsetEls.length; i++) {
-      fieldsetEl = fieldsetEls[i];
+    for (let i = 0; i < fieldsetsEl.length; i++) {
+      fieldsetEl = fieldsetsEl[i];
       fieldsetEl.setAttribute('disabled', true);
     }
   };
+
+  const validateTitle = function() {
+    if (titleEl.validity.valueMissing) {
+      titleEl.setCustomValidity('Введите заголовок!');
+      titleEl.style.borderColor = 'red';
+    } else if (titleEl.validity.tooShort) {
+      titleEl.setCustomValidity('Слишком короткий заголовок - минимум 30 символов!');
+      titleEl.style.borderColor = 'red';
+    } else if (titleEl.validity.tooLong) {
+      titleEl.setCustomValidity('Слишком длинный заголовок - не больше 100 символов!');
+      titleEl.style.borderColor = 'red';
+    } else {
+      titleEl.setCustomValidity('');
+      titleEl.style.borderColor = INPUT_DEFAULT_BORDER_COLOR;
+    }
+  };
+
+  const validatePrice = function() {
+    if (priceEl.validity.valueMissing) {
+      priceEl.setCustomValidity('Укажите цену');
+      priceEl.style.borderColor = 'red';
+    } else if (priceEl.validity.rangeOverflow) {
+      priceEl.setCustomValidity('Слишком много! Цена не должна быть выше 1 млн');
+      priceEl.style.borderColor = 'red';
+    } else if (priceEl.validity.rangeUnderflow) {
+      priceEl.setCustomValidity('Маловато! Минимальная цена: ' + priceEl.min);
+      priceEl.style.borderColor = 'red';
+    } else {
+      priceEl.setCustomValidity('');
+      priceEl.style.borderColor = INPUT_DEFAULT_BORDER_COLOR;
+    }
+  };
+
+  // Sync checkin fields
+  timeinEl.addEventListener('change', function() {
+    timeoutEl.value = timeinEl.value;
+  });
+  timeoutEl.addEventListener('change', function() {
+    timeinEl.value = timeoutEl.value;
+  });
+
+  // Validate fields
+  titleEl.addEventListener('keyup', function() {
+    validateTitle();
+  });
+  priceEl.addEventListener('keyup', function() {
+    validatePrice();
+  });
 
   window.form = {
     enableForm: enableForm,
--- js/show-card.js
+++ js/show-card.js
@@ -3,6 +3,10 @@
 (function() {
   const mapFiltersEl = document.querySelector('.map__filters-container');
   const mapCardTemplate = document.querySelector('template').content.querySelector('.map__card');
+  const photoDimension = {
+    WIDTH: 70,
+    HEIGHT: 70
+  };
 
   const getFeatures = function(data) {
     let features = document.createDocumentFragment();
@@ -16,12 +20,53 @@
     return features;
   };
 
+  const getPhotos = function(data) {
+    let photos = document.createDocumentFragment();
+
+    data.forEach(function(item) {
+      let photoItem = document.createElement('li');
+      let photoImg = document.createElement('img');
+
+      photoImg.src = item;
+      photoImg.width = photoDimension.WIDTH;
+      photoImg.height = photoDimension.HEIGHT;
+      photoItem.appendChild(photoImg);
+      photos.appendChild(photoItem);
+    });
+
+    return photos;
+  };
+
+  const pluralizeRooms = function(roomsNumber) {
+    switch (roomsNumber) {
+      case 1:
+        return roomsNumber + ' комната для ';
+      case 0:
+      case 5:
+        return roomsNumber + ' комнат для ';
+      default:
+        return roomsNumber + ' комнаты для ';
+    }
+  };
+
+  const pluralizeGuests = function(guestsNumber) {
+    switch (guestsNumber) {
+      case 0:
+      case 1:
+        return guestsNumber + ' гостя';
+      default:
+        return guestsNumber + ' гостей';
+    }
+  };
+
   const closePopup = function() {
     let popup = document.querySelector('.map__card');
 
     if (popup) {
       popup.parentNode.removeChild(popup);
     }
+
+    document.removeEventListener('keydown', cardEscKeydownHandler);
   };
 
   const cardCloseClickHandler = function() {
@@ -32,6 +77,10 @@
     window.util.isEnterEvent(evt, closePopup);
   };
 
+  const cardEscKeydownHandler = function(evt) {
+    window.util.isEscEvent(evt, closePopup);
+  };
+
   const renderPopup = function(card) {
     closePopup();
 
@@ -39,10 +88,12 @@
 
     offerCard.querySelector('.popup__title').textContent = card.offer.title;
     offerCard.querySelector('.popup__address').textContent = card.offer.adress;
-    offerCard.querySelector('.popup__price').textContent = card.offer.price;
+    offerCard.querySelector('.popup__price').textContent = card.offer.price + ' \u20BD/ночь';
     offerCard.querySelector('.popup__type').textContent = card.offer.type;
     offerCard.querySelector('.popup__description').textContent = card.offer.description;
-    offerCard.querySelector('.popup__accommodation').textContent = card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
+    offerCard.querySelector('.popup__accommodation').textContent = pluralizeRooms(card.offer.rooms) + pluralizeGuests(card.offer.guests);
+    offerCard.querySelector('.popup__schedule').textContent = 'Заезд после ' + card.offer.checkin + ', выезд до ' + card.offer.checkout;
+    offerCard.querySelector('.popup__avatar').src = card.author.avatar;
 
     // Render features
     const featuresList = offerCard.querySelector('.popup__features');
@@ -56,6 +107,16 @@
       featuresList.parentNode.removeChild(featuresList);
     }
 
+    // Render pictures
+    const photosList = offerCard.querySelector('.popup__pictures');
+
+    if (card.offer.photos !== 0) {
+      while (photosList.firstChild) {
+        photosList.removeChild(photosList.firstChild);
+      }
+      photosList.appendChild(getPhotos(card.offer.photos));
+    }
+
     // Insert card in DOM
     document.querySelector('.map').insertBefore(offerCard, mapFiltersEl);
 
@@ -64,6 +125,9 @@
 
     cardCloseEl.addEventListener('click', cardCloseClickHandler);
     cardCloseEl.addEventListener('keydown', cardCloseKeydownHandler);
+
+    // ESC keydown handler
+    document.addEventListener('keydown', cardEscKeydownHandler);
   };
 
   window.showCard = {
